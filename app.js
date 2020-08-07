ca5d5d67defe8c4edceaeefdbb2b984473c13a09
Gamygams1234 <gamyburgos@gmail.com>
2020-08-07 00:41:52 -0700

    Capturing al errors

--- app.js
+++ app.js
@@ -10,32 +10,43 @@ function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
   console.log(message);
 }
-function getProfile(username) {
-  // Connect to the API URL (https://teamtreehouse.com/username.json)
-  try {
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
-      let body = "";
-      // Read the data
-      // making it to string so it doesn't return a buffer
-      response.on("data", (data) => {
-        body += data.toString();
-      });
 
-      response.on("end", () => {
-        const profile = JSON.parse(body);
-        printMessage(username, profile.badges.length, profile.points.JavaScript);
-        // right now it is passing a string
-
-        // Parse the data
-        // Print the data
-      });
-    });
-    request.on("error", (error) => console.error(`Problem with request: ${error.message}`));
-  } catch (error) {
-    console.error(error.message);
-  }
+function printError(error) {
+  console.error(error.message);
 }
 
+function getProfile(username) {
+  try {
+    // Connect to the API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
+      if (response.statusCode === 200) {
+        let body = "";
+        // Read the data
+        response.on("data", (data) => {
+          body += data.toString();
+        });
+
+        response.on("end", () => {
+          try {
+            // Parse the data
+            const profile = JSON.parse(body);
+            // Print the data
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+          } catch (error) {
+            printError(error);
+          }
+        });
+      } else {
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
+    });
+    request.on("error", printError);
+  } catch (error) {
+    printError(error);
+  }
+}
 const users = ["chalkers", "Francisco"];
 users.forEach((user) => {
   getProfile(user);
